ee60fd446bc56e3279395b91ed6c1ae283f238ed
Pialex99 <33020819+Pialex99@users.noreply.github.com>
2020-04-07 14:26:01 +0200

    The Ball bounce on the wall !!

--- game/Scripts/Ball.gd
+++ game/Scripts/Ball.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 var velocity
 var screen_size
+export var min_speed = 150
+export var max_speed = 200
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -13,10 +15,23 @@ func _ready():
 	position = Vector2(screen_size.x / 2, screen_size.y / 2)
 	velocity = Vector2(1, 0)
 	var direction = rand_range(-PI, PI)
-	velocity = velocity.rotated(direction)
+	velocity = velocity.rotated(direction) * rand_range(min_speed, max_speed)
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position = position + velocity * delta
+	if (position.x > screen_size.x):
+		position.x = screen_size.x
+		velocity.x = -velocity.x
+	elif (position.x < 0):
+		position.x = 0
+		velocity.x = -velocity.x
+	
+	if (position.y > screen_size.y):
+		position.y = screen_size.y
+		velocity.y = -velocity.y
+	elif (position.y < 0):
+		position.y = 0
+		velocity.y = -velocity.y
