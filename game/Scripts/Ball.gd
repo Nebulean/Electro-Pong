75ec4fd3ba4ad228081d7da9d6d6e68f5d524ca3
Pialex99 <33020819+Pialex99@users.noreply.github.com>
2020-04-07 14:13:51 +0200

    The ball moves now !!

--- game/Scripts/Ball.gd
+++ game/Scripts/Ball.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var velocity
+var screen_size
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -7,9 +9,14 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
+	position = Vector2(screen_size.x / 2, screen_size.y / 2)
+	velocity = Vector2(1, 0)
+	var direction = rand_range(-PI, PI)
+	velocity = velocity.rotated(direction)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	position = position + velocity * delta
