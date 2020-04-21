edf218da52ad403a8977995b6dda881f06b6458f
Damien Korber <damien@korber.email>
2020-04-21 09:45:19 +0200

    Added debut print to _ready function
    Co-Authored-By: Mojert <hamza.meel@epfl.ch>

--- game/Scripts/Ball.gd
+++ game/Scripts/Ball.gd
@@ -10,7 +10,7 @@ export var manual_state = false
 
 
 func _ready():
-	print("readying")
+	print_debug("readying")
 	if not manual_state:
 		screen_size = get_viewport_rect().size
 		position = Vector2(screen_size.x / 2, screen_size.y / 2)
