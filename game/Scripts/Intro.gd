1a4a7578c9c317455a3df73d2aefa8c3ebe0d56b
Damien Korber <damien@korber.email>
2020-04-21 13:54:11 +0200

    Fixed missing first line

--- game/Scripts/Intro.gd
+++ game/Scripts/Intro.gd
@@ -33,13 +33,12 @@ func _physics_process(delta):
 func _input(ev):
 	# We check if any key is pressed
 	if ev is InputEventKey and ev.pressed:
-		textIndex += 1
-		
 		# If we are at the end of the file, we go to next scene
 		if textIndex >= text.size():
 			get_tree().change_scene("res://Scenes/Gameplay.tscn")
 		else:
 			next_content(textIndex)
+		textIndex += 1
 
 # Will handle what append when next line
 func next_content(index):
