03afc59efbb5a66076004b279a22a11994ef74bc
Damien Korber <damien@korber.email>
2020-04-28 14:51:22 +0200

    Modified position of powerup and added blank execute fonction

--- game/Scripts/PowerupStorage.gd
+++ game/Scripts/PowerupStorage.gd
@@ -2,26 +2,39 @@ extends Node2D
 
 var current # stores the current powerup (empty initially)
 enum {MAGNETIC, POLARITY, ELECTRO_OUT, ELECTRO_IN} # powerups type names
-var mag
-var pol
-var el_out
-var el_in
+var mag_scn
+var pol_scn
+var el_out_scn
+var el_in_scn
 
 func _ready():
 	pass
-	# mag		= preload("res://Scenes/")
-	# pol		= preload("res://Scenes")
-	# el_out		= preload("res://Scenes/")
-	# el_in		= preload("res://Scenes")
+	# mag_scn		= preload("res://Scenes/")
+	# pol_scn		= preload("res://Scenes")
+	# el_out_scn		= preload("res://Scenes/")
+	# el_in_scn		= preload("res://Scenes")
 	
 func spawn(type):
+	# We remove the previous powerup
 	if not current.empty:
-		pass
+		current.queue_free()
+	# And we add the new one.
 	if type == MAGNETIC:
-		pass
-	if type == POLARITY:
-		pass
-	if type == ELECTRO_OUT:
-		pass
-	if type == ELECTRO_IN:
-		pass
+		current = mag_scn.instance()
+	elif type == POLARITY:
+		current = pol_scn.instance()
+	elif type == ELECTRO_OUT:
+		current = el_out_scn.instance()
+	elif type == ELECTRO_IN:
+		current = el_in_scn.instance()
+	else:
+		print_debug("This is not a valid powerup type.")
+	
+	# We disable the powerup (it's just there, nothing more).
+		
+	# We add the new powerup to the storage
+	add_child(current)
+
+func execute_powerup():
+	#current.
+	pass
