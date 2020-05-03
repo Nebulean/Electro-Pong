91137ab99d30803d72ad918110784201a50741b2
Damien Korber <damien@korber.email>
2020-05-03 19:44:59 +0200

    I don't know how, but I fixed the colliding pads problem.

--- game/Scripts/Player.gd
+++ game/Scripts/Player.gd
@@ -12,8 +12,12 @@ var player
 var ring_center: Vector2
 var ring_radius: float
 var score := 0 setget , get_score
-
+var is_ready_to_collide
+var is_colliding_with_player = false
+var was_trigo = false
+var was_clockwise = false
 signal player_won(player)
+
 
 func set_player(num: int, center: Vector2, radius: float):
 	assert(num in [1, 2])
@@ -28,6 +32,8 @@ func set_player(num: int, center: Vector2, radius: float):
 	ring_center = center
 	ring_radius = radius
 	position = position_from_angle(angle)
+	
+
 
 func position_from_angle(alpha: float) -> Vector2:
 	return ring_center + ring_radius * Vector2(cos(alpha), sin(alpha))
@@ -53,10 +59,19 @@ func _input(event):
 
 func _physics_process(delta):
 	var lastAngle = angle
-	if is_clockwise:
-		angle = wrapf(angle + angle_step * delta, 0, 2*PI)
-	if is_trigo:
-		angle = wrapf(angle - angle_step * delta, 0, 2*PI)
+	
+	if !is_colliding_with_player:
+		if is_clockwise:
+			angle = wrapf(angle + angle_step * delta, 0, 2*PI)
+		if is_trigo:
+			angle = wrapf(angle - angle_step * delta, 0, 2*PI)
+	else:
+		if is_clockwise and !was_clockwise:
+			angle = wrapf(angle + angle_step * delta, 0, 2*PI)
+		if is_trigo and !was_trigo:
+			angle = wrapf(angle - angle_step * delta, 0, 2*PI)
+
+
 	position = position_from_angle(angle)
 	rotation = angle + PI/2
 
@@ -67,3 +82,32 @@ func increment_score():
 
 func get_score():
 	return score
+
+# This will detect the collision between the player and other stuff.
+# If it collides with another player, they will be blocked.
+func _on_Area2D_body_entered(body):
+	# if only one of the keys is pressed
+	if (!is_colliding_with_player and (is_clockwise and !is_trigo) or (!is_clockwise and is_trigo)):
+		get_tree().call_group("players", "tell_colliding", body, is_clockwise, is_trigo, player)
+
+func tell_colliding(body, is_cw, is_tr, source):
+	if source == player:
+		was_clockwise = is_cw
+		was_trigo = is_tr
+	else:
+		was_clockwise = !is_cw
+		was_trigo = !is_tr
+	if body.is_in_group("players") and is_ready_to_collide:
+		is_colliding_with_player = true
+		print_debug("The two players are colliding.")
+
+func _on_Area2D_body_exited(body):
+	get_tree().call_group("players", "tell_colliding", body)
+	was_clockwise = is_clockwise
+	was_trigo = is_trigo
+	if body.is_in_group("players"):
+		is_colliding_with_player = false
+
+# simple timer initial pour éviter la collision initiale qui va bloquer les joueurs.
+func _on_Timer_timeout():
+	is_ready_to_collide = true # Replace with function body.
