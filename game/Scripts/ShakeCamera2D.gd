5fd0a068e4437d7392d524115edd95b021a88e35
Florian <florian.rieder@unil.ch>
2020-05-05 12:00:58 +0200

    Added screen shake
    Triggers on:
    - ball colliding with pads (0.3 trauma)
    - ball exiting the ring (0.6 trauma)

---
+++ game/Scripts/ShakeCamera2D.gd
@@ -0,0 +1,50 @@
+extends Camera2D
+class_name ShakeCamera2D
+
+export var decay = 0.8  # How quickly the shaking stops [0, 1].
+export var max_offset = Vector2(100, 50)  # Maximum hor/ver shake in pixels.
+export var max_roll = 0.1  # Maximum rotation in radians (use sparingly).
+#export (NodePath) var target  # Assign the node this camera will follow.
+
+var trauma = 0.0  # Current shake strength.
+var trauma_power = 2  # Trauma exponent. Use [2, 3].
+onready var noise = OpenSimplexNoise.new()
+var noise_y = 0
+
+export var ball_pad_collision_trauma = 0.3
+export var ring_exit_trauma = 0.6
+
+func _ready():
+	randomize()
+	noise.seed = randi()
+	noise.period = 4
+	noise.octaves = 2
+
+func _process(delta):
+	#if target:
+	#	global_position = get_node(target).global_position
+	if trauma:
+		trauma = max(trauma - decay * delta, 0)
+		shake()
+		
+func shake():
+	var amount = pow(trauma, trauma_power)
+	noise_y += 1
+	# Using noise
+	rotation = max_roll * amount * noise.get_noise_2d(noise.seed, noise_y)
+	offset.x = max_offset.x * amount * noise.get_noise_2d(noise.seed*2, noise_y)
+	offset.y = max_offset.y * amount * noise.get_noise_2d(noise.seed*3, noise_y)
+	# Pure randomness
+#	rotation = max_roll * amount * rand_range(-1, 1)
+#	offset.x = max_offset.x * amount * rand_range(-1, 1)
+#	offset.y = max_offset.y * amount * rand_range(-1, 1)
+	
+func add_trauma(amount):
+	trauma = min(trauma + amount, 1.0)
+
+func _on_Ball_body_entered(body):
+	add_trauma(ball_pad_collision_trauma)
+
+
+func _on_Ring_body_exited(body):
+	add_trauma(ring_exit_trauma)
