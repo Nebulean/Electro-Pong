80f413e598b4646f31c7c084ae0bd802477f232c
Damien Korber <damien@korber.email>
2020-05-12 13:36:29 +0200

    Fixed collision with powerups

--- game/Scripts/PowerUpElecAtt.gd
+++ game/Scripts/PowerUpElecAtt.gd
@@ -3,7 +3,8 @@ extends Area2D
 signal taken_elec_att
 
 func _on_PowerUpElecAtt_body_entered(body):
-	hide()
-	emit_signal("taken_elec_att")
-	$CollisionShape2D.set_deferred("disabled", true)
-	queue_free()
+	if body.is_in_group("balls"):
+		hide()
+		emit_signal("taken_elec_att")
+		$CollisionShape2D.set_deferred("disabled", true)
+		queue_free()
--- game/Scripts/PowerUpMagnetic.gd
+++ game/Scripts/PowerUpMagnetic.gd
@@ -3,7 +3,8 @@ extends Area2D
 signal taken_magnetic
 
 func _on_PowerUpMagnetic_body_entered(body):
-	hide()
-	emit_signal("taken_magnetic")
-	$CollisionShape2D.set_deferred("disabled", true)
-	queue_free()
+	if body.is_in_group("balls"):
+		hide()
+		emit_signal("taken_magnetic")
+		$CollisionShape2D.set_deferred("disabled", true)
+		queue_free()
